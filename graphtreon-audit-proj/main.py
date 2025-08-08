db637909d474a6437c9583e320b3aa5056c61ccd
Kamila <kamillashirinova9r1@gmail.com>
2025-08-08 20:13:15 +0200

    Update main.py
    added graphs

--- graphtreon-audit-proj/main.py
+++ graphtreon-audit-proj/main.py
@@ -5,7 +5,7 @@ import cloudscraper, re, json
 
 URL = "https://graphtreon.com/creator/lazytarts"
 
-scraper = cloudscraper.create_scraper()     # handles JS challenge automatically
+scraper = cloudscraper.create_scraper()     
 html     = scraper.get(URL, timeout=30).text
 daily_patrons_pattern  = re.compile( r'dailyGraph_patronSeriesData\s*=\s*(\[[\s\S]*?\])\s*;', re.DOTALL | re.DOTALL)
 daily_earnings_pattern  = re.compile( r'dailyGraph_earningsSeriesData\s*=\s*(\[[\s\S]*?\])\s*;', re.DOTALL| re.DOTALL)
@@ -23,3 +23,17 @@ earnings_df = pd.DataFrame(daily_earning_series, columns=["timestamp_ms", "daily
 earnings_df["date"] = pd.to_datetime(earnings_df["timestamp_ms"], unit="ms", utc=True).dt.date
 
 print(patrons_df)
+
+plt.figure(1)
+plt.plot(patrons_df["date"], patrons_df["paid_members"])
+plt.xlabel('date')
+plt.ylabel('paid_members')
+plt.title('All time daily patron counts')
+
+plt.figure(2)
+plt.plot(earnings_df["date"], earnings_df["daily_earnings"])
+plt.xlabel('date')
+plt.ylabel('daily_earnings')
+plt.title('All time daily revenue')
+
+plt.show()
